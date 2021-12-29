cc5e84ef2b7674611d80b9146483ebc557033c8d
kdjay517 <kdjay517@gmail.com>
2021-12-29 14:47:32 +0530

    Added: Taking multiple rides and calculating the aggregate total for all

--- src/main/java/com/bridgelabz/cabinvoice/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoice/CabInvoiceGenerator.java
@@ -1,12 +1,22 @@
 package com.bridgelabz.cabinvoice;
 
+import com.bridgelabz.model.Ride;
+
 public class CabInvoiceGenerator {
-    private static final double COST_PER_KM = 10;
-    private static final double COST_PER_MIN = 1;
-    private static final double MIN_FARE = 5.0;
+    public static final double COST_PER_KM = 10;
+    public static final int COST_PER_MIN = 1;
+    public static final double MIN_Fare = 5.0;
 
     public double calculateFare(double distance, int time) {
         double totalFare = distance * COST_PER_KM + time * COST_PER_MIN;
-        return Math.max(totalFare, MIN_FARE);
+        return Math.max(totalFare, MIN_Fare);
+    }
+    public double calculateFare(Ride[] rides) {
+        double totalFare = 0.0;
+        for (Ride ride : rides) {
+            totalFare += calculateFare(ride.getDistance(), ride.getTime());
+        }
+        return totalFare;
+
     }
 }
